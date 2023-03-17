1c3e58cc8b64da111cf3bef2723f321c492badab
Caio <caio333119@gmail.com>
2023-03-17 19:59:00 -0300

    Add files via upload

---
+++ Bleach Versus Crusade/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Bleach_Versus_Crusade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Extrair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Inserir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Bleach_Versus_Crusade.Properties.Resources.back;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(422, 175);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "DAT TEXT Extractor and Inserter By Angel333119";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Bleach Versus Crusade/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bleach_Versus_Crusade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Arquivo DAT|*.DAT|Todos os arquivos (*.*)|*.*";
+            openFileDialog1.Title = "Escolha um arquivo DAT do jogo Bleach: Versus Crusade...";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    using (FileStream stream = File.Open(file, FileMode.Open))
+                    {
+                        BinaryReader br = new BinaryReader(stream);
+                        BinaryWriter bw = new BinaryWriter(stream);
+
+                        long tamanhoarquivo = new FileInfo(file).Length;
+
+                        uint quantidadedeponteiros = Converterendian.bigendian32(br.ReadUInt32()) / 4;
+
+                        br.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                        uint[] ponteiro = new uint[quantidadedeponteiros];
+
+                        for (int i = 0; i < quantidadedeponteiros; i++)
+                        {
+                            ponteiro[i] = Converterendian.bigendian32(br.ReadUInt32());
+                        }
+
+                        uint[] tamanhodotexto = new uint[quantidadedeponteiros];
+
+                        string todosostextos = "";
+
+                        for (int i = 0; i < quantidadedeponteiros; i++)
+                        {
+                            br.BaseStream.Seek(ponteiro[i], SeekOrigin.Begin);
+
+                            if (i < quantidadedeponteiros - 1)
+                            {
+                                tamanhodotexto[i] = ponteiro[i + 1] - ponteiro[i];
+                            }
+                            else
+                            {
+                                tamanhodotexto[i] = (uint)(tamanhoarquivo - ponteiro[i]);
+                            }
+
+                            byte[] bytestext = new byte[tamanhodotexto[i]];
+
+                            for (int j = 0; j < tamanhodotexto[i]; j++)
+                            {
+                                bytestext[j] = br.ReadByte();
+                            }
+                            
+                            string textodecodificado = Encoding.BigEndianUnicode.GetString(bytestext);
+
+                            if (bytestext.SequenceEqual(new byte[] { 48, 0, 0, 0 }))
+                            {
+                                textodecodificado = "<48><00>";
+                            }
+
+                            todosostextos += textodecodificado.Replace("\0", String.Empty) + "\r\n";
+
+                            File.WriteAllText(Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)) + ".txt", todosostextos);
+
+                        }
+                    }
+                }
+                MessageBox.Show("Texto extraido!", "AVISO!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Arquivo DAT|*.DAT|Todos os arquivos (*.*)|*.*";
+            openFileDialog1.Title = "Escolha um arquivo DAT do jogo Bleach: Versus Crusade...";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    FileInfo dump = new FileInfo(Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)) + ".txt");
+
+                    if (dump.Exists)
+                    {
+                        using (FileStream stream = File.Open(file, FileMode.Open))
+                        {
+                            BinaryReader br = new BinaryReader(stream);
+                            BinaryWriter bw = new BinaryWriter(stream);
+
+                            var txt = File.ReadLines(Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)) + ".txt");
+
+                            long filesize = new FileInfo(file).Length;
+
+                            uint primeiroponteiro = Converterendian.bigendian32(br.ReadUInt32());
+
+                            stream.SetLength(primeiroponteiro);
+
+                            int contador = 0;
+
+                            uint novoponteiro = primeiroponteiro;
+
+                            try
+                            {
+                                foreach (var linha in txt)
+                                {
+                                    bw.BaseStream.Seek(4 * contador, SeekOrigin.Begin);
+
+                                    bw.Write(Converterendian.bigendian32(novoponteiro));
+
+                                    bw.BaseStream.Seek(novoponteiro, SeekOrigin.Begin);
+
+                                    byte[] bytes = new byte[0];
+
+                                    if (linha == "<48><00>")
+                                    {
+                                        string hex = "3000";
+                                        hex = hex.Replace("<", "").Replace(">", "");
+                                        bytes = new byte[2];
+                                        bytes[0] = Convert.ToByte(hex.Substring(0, 2), 16);
+                                        bytes[1] = Convert.ToByte(hex.Substring(2, 2), 16);
+                                    }
+                                    else
+                                    {
+                                        bytes = Encoding.BigEndianUnicode.GetBytes(linha);
+                                    }                                    
+
+                                    bw.BaseStream.Seek(novoponteiro, SeekOrigin.Begin);
+
+                                    if (string.IsNullOrWhiteSpace(linha))
+                                    {
+                                        bw.Write((short)0);
+                                    }
+                                    else
+                                    {
+                                        bw.Write(bytes);
+                                    }
+
+                                    novoponteiro = novoponteiro + (uint)bytes.Length + 2;
+
+                                    contador++;
+                                }
+                            }
+                            catch (System.IndexOutOfRangeException)
+                            {
+
+                            }
+                            bw.Write((Int16)0);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("O arquivo TXT não foi encontrado.\n\nO arquivo TXT deve estar na mesma pasta do arquivo DAT.", "AVISO!");
+                    }
+                }
+                MessageBox.Show("Texto inserido.", "AVISO!");
+            }
+        }
+    }
+}
